80c99057f5c875d002c52578d14871daa4470adf
Patrik Larsson <patrik@larn.se>
2015-07-01 16:26:21 +0200

    Antal medlemmar

--- resources/views/content/foreningen.blade.php
+++ resources/views/content/foreningen.blade.php
@@ -3,7 +3,7 @@
         <div class="large-8 columns about-content">
             <h2>Om föreningen</h2>
 
-            <p>Nordskens Vänner är en ideell förening ansluten till Sveriges största ungdomsförbund, <a href="//www.sverok.se">Sverok</a>. Föreningen har cirka 5-600 medlemmar, främst från Skellefteå med omnejd.</p>
+            <p>Nordskens Vänner är en ideell förening ansluten till Sveriges största ungdomsförbund, <a href="//www.sverok.se">Sverok</a>. Föreningen har mellan 500-600 medlemmar, främst från Skellefteå med omnejd.</p>
             <p>Föreningen har sedan 2011 fokuserat på spelfestivalen Nordsken, vilket även fortsättningsvis är vårt primära fokus. Förhoppningen är dock att anordna ytterligare ett par aktiviteter under verksamhetsåret tillsammans med våra medlemmar för att främja spelhobbyn i Skellefteå.</p>
 
             <p>Vill du bli medlem och engagera dig? <a href="#kontakt">Kontakta oss</a> så får du mer information!</p>
