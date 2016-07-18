f64b30c97c52ea441d66163e27f3af09cd077ea2
Patrik Larsson <patrik@larn.se>
2016-07-18 13:12:16 +0200

    Update styrelsen.blade.php

--- resources/views/content/styrelsen.blade.php
+++ resources/views/content/styrelsen.blade.php
@@ -69,4 +69,17 @@
             </ul>
         </div>
     </div>
+    
+    <div class="row">
+        <div class="large-8 columns about-content">
+            <h4>Dick Hellman <small>Revisor</small></h4>
+            <span class="contact">
+                <i class="fa fa-envelope"></i> <a href="mailto:dick@nordsken.se">dick@nordsken.se</a>
+            </span>
+        </div>
+        <div class="large-4 columns">
+            <ul class="small-block-grid-2">
+            </ul>
+        </div>
+    </div>
 </section>
