f8a58855cf16b3a5c6895aae8cf68acf20819516
Patrik Larsson <patrik@nordsken.se>
2018-04-17 19:45:09 +0200

    Update board members.

--- resources/views/content/styrelsen.blade.php
+++ resources/views/content/styrelsen.blade.php
@@ -6,10 +6,24 @@
     </div>
     <div class="row">
         <div class="large-8 columns about-content">
-            <h4>Patrik Larsson <small>Ordförande</small></h4>
+            <h4>Linn-Marie Edlund <small>Ordförande</small></h4>
             <span class="contact">
-                <i class="fa fa-envelope"></i> <a href="mailto:patrik@nordsken.se">patrik@nordsken.se</a><br>
-                <i class="fa fa-phone"></i> <a href="tel:+46707107181">070-710 71 81</a>
+                <i class="fa fa-envelope"></i> <a href="mailto:linnmarie@nordsken.se">linnmarie@nordsken.se</a><br>
+                <i class="fa fa-phone"></i> <a href="tel:+46730336791">073-033 67 91</a>
+            </span>
+        </div>
+        <div class="large-4 columns">
+            <ul class="small-block-grid-2">
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="large-8 columns about-content">
+            <h4>Johan Linder <small>Vice ordförande</small></h4>
+            <span class="contact">
+                <i class="fa fa-envelope"></i> <a href="mailto:johan@nordsken.se">johan@nordsken.se</a>
             </span>
         </div>
         <div class="large-4 columns">
@@ -20,9 +34,9 @@
 
     <div class="row">
         <div class="large-8 columns about-content">
-            <h4>Johan Linder <small>Kassör</small></h4>
+            <h4>Anita Vidman <small>Kassör</small></h4>
             <span class="contact">
-                <i class="fa fa-envelope"></i> <a href="mailto:johan@nordsken.se">johan@nordsken.se</a><br>
+                <i class="fa fa-envelope"></i> <a href="mailto:kassor@nordsken.se">kassor@nordsken.se</a><br>
             </span>
         </div>
         <div class="large-4 columns">
@@ -33,9 +47,9 @@
 
     <div class="row">
         <div class="large-8 columns about-content">
-            <h4>Andreas Eriksson <small>Sekreterare</small></h4>
+            <h4>Patrik Larsson <small>Sekreterare</small></h4>
             <span class="contact">
-                <i class="fa fa-envelope"></i> <a href="mailto:andreas@nordsken.se">andreas@nordsken.se</a>
+                <i class="fa fa-envelope"></i> <a href="mailto:patrik@nordsken.se">patrik@nordsken.se</a>
             </span>
         </div>
         <div class="large-4 columns">
@@ -44,24 +58,11 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="large-8 columns about-content">
-            <h4>Linn-Marie Edlund <small>Ledamot</small></h4>
-            <span class="contact">
-                <i class="fa fa-envelope"></i> <a href="mailto:linnmarie.edlund@nordsken.se">linnmarie.edlund@nordsken.se</a>
-            </span>
-        </div>
-        <div class="large-4 columns">
-            <ul class="small-block-grid-2">
-            </ul>
-        </div>
-    </div>
-    
     <div class="row">
         <div class="large-8 columns about-content">
             <h4>Olov Berglund <small>Ledamot</small></h4>
             <span class="contact">
-                <i class="fa fa-envelope"></i> <a href="mailto:olov.berglund@nordsken.se">olov.berglund@nordsken.se</a>
+                <i class="fa fa-envelope"></i> <a href="mailto:olov@nordsken.se">olov@nordsken.se</a>
             </span>
         </div>
         <div class="large-4 columns">
@@ -69,7 +70,33 @@
             </ul>
         </div>
     </div>
-    
+
+    <div class="row">
+        <div class="large-8 columns about-content">
+            <h4>Isa Blomberg <small>Ledamot</small></h4>
+            <span class="contact">
+                <i class="fa fa-envelope"></i> <a href="mailto:isa@nordsken.se">isa@nordsken.se</a>
+            </span>
+        </div>
+        <div class="large-4 columns">
+            <ul class="small-block-grid-2">
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="large-8 columns about-content">
+            <h4>Oskar Svanberg <small>Ledamot</small></h4>
+            <span class="contact">
+                <i class="fa fa-envelope"></i> <a href="mailto:oskar@nordsken.se">oskar@nordsken.se</a>
+            </span>
+        </div>
+        <div class="large-4 columns">
+            <ul class="small-block-grid-2">
+            </ul>
+        </div>
+    </div>
+
     <div class="row">
         <div class="large-8 columns about-content">
             <h4>Dick Hellman <small>Revisor</small></h4>
